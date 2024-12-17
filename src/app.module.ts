03c508feaaa005a101522f8f3397b2a53c569eaa
Jean Lima <jeansantoslima17@gmail.com>
2024-12-17 16:28:23 -0300

    feat: add Stripe module to app imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
 import { DatabaseService } from './services/Database.service';
+import { StripeModule } from './modules/stripe/stripe.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, StripeModule],
   controllers: [AppController],
   providers: [AppService],
 })
