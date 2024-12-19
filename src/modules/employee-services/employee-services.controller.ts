3d32a9a62cabb1373abd9865697a293cae5e379f
Jean Lima <jeansantoslima17@gmail.com>
2024-12-19 12:19:33 -0300

    feat: implement role-based access control for employee services

--- src/modules/employee-services/employee-services.controller.ts
+++ src/modules/employee-services/employee-services.controller.ts
@@ -1,6 +1,9 @@
 import { Body, Controller, Get, Param, ParseIntPipe, Post, UseGuards } from "@nestjs/common";
 import { EmployeeServicesService } from "./employee-services.service";
 import { JwtAuthGuard } from "src/guards/jwt-auth.guard";
+import { Role } from "@prisma/client";
+import { Roles } from "src/decorators/Roles.decorator";
+import { RoleGuard } from "src/guards/roles.guard";
 
 @Controller('employee-services')
 export class EmployeeServicesController {
@@ -8,7 +11,8 @@ export class EmployeeServicesController {
         private readonly employeeServicesService: EmployeeServicesService,
     ) {}
 
-    @UseGuards(JwtAuthGuard)
+    @Roles([Role.ADMIN])
+    @UseGuards(JwtAuthGuard, RoleGuard)
     @Post()
     async create(@Body('employeeId') employeeId: number, @Body('serviceId') serviceId: number) {
         return await this.employeeServicesService.create(employeeId, serviceId);
