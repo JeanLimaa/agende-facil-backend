4fc607c04d3d281f36c1640ad45349821e953dd7
Jean Lima <jeansantoslima17@gmail.com>
2025-01-15 12:09:14 -0300

    feat: update CreateAppointmentDto to enforce date format and add validation for hours and employee/service IDs

--- src/modules/appointments/dto/create-appointment.dto.ts
+++ src/modules/appointments/dto/create-appointment.dto.ts
@@ -1,13 +1,22 @@
-import { IsString, IsEmail, IsNotEmpty } from 'class-validator';
+import { IsString, IsNotEmpty, IsNumber, IsDateString } from 'class-validator';
 
 export class CreateAppointmentDto {
-    date: Date;
+    //@IsNotEmpty()
+    @IsDateString()
+    date: string;
+
+    @IsString({message: "Hora precisa ser uma string. Ex: 08:00"})
+    hours: string;
 
     clientId?: number;
 
     guestClientId?: number;
 
+    //@IsNumber()
+    @IsNotEmpty({message: "ID do empregado não informado."})
     employeeId: number;
     
+    //@IsNumber()
+    @IsNotEmpty({message: "ID do serviço não informado."})
     serviceId: number;
 }
