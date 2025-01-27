1b2234c39e4669898f9d7e1343f1168f599e9f1f
Jean Lima <jeansantoslima17@gmail.com>
2025-01-27 17:42:48 -0300

    feat: add validation for appointment scheduling within employee working hours

--- src/modules/appointments/appointment.service.ts
+++ src/modules/appointments/appointment.service.ts
@@ -2,6 +2,8 @@ import { Injectable, BadRequestException } from '@nestjs/common';
 import { DatabaseService } from 'src/services/Database.service';
 import { Appointment, Status } from '@prisma/client';
 import { CreateAppointmentDto } from './dto/create-appointment.dto';
+import { isAfter, isBefore, parseISO, set } from 'date-fns';
+import { is } from 'date-fns/locale';
 
 @Injectable()
 export class AppointmentService {
@@ -20,6 +22,31 @@ export class AppointmentService {
 
     if (existingAppointment) {
       throw new BadRequestException('Funcionário já tem outro agendamento nesse horário.');
+    }
+
+    // verifica se o agendamento está no horario de atendimento do funcionario
+    const employee = await this.prisma.employee.findUnique({
+      where: { id: data.employeeId },
+    });
+    
+    if(!employee) throw new BadRequestException('Funcionário não encontrado.');
+
+    // Criar os horários de início e fim como objetos Date
+    const appointmentDate = parseISO(data.date);
+    const startHour = set(appointmentDate, {
+      hours: parseInt(employee.startHour.split(':')[0], 10),
+      minutes: parseInt(employee.startHour.split(':')[1], 10),
+      seconds: 0,
+    });
+
+    const endHour = set(appointmentDate, {
+      hours: parseInt(employee.endHour.split(':')[0], 10),
+      minutes: parseInt(employee.endHour.split(':')[1], 10),
+      seconds: 0,
+    });
+
+    if(isBefore(appointmentDate, startHour) || isAfter(appointmentDate, endHour)) {
+      throw new BadRequestException('Funcionário não atende nesse horário.');
     }
 
     const appointment = await this.prisma.appointment.create({
