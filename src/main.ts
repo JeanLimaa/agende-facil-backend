a0e5c5173334f8e0a457739d4aa67e3a195806ee
Jean Lima <jeansantoslima17@gmail.com>
2025-06-10 16:36:06 -0300

    feat(validation): refactor error handling in ValidationPipe to use findFirstConstraint function

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { BadRequestException, Logger, ValidationPipe } from '@nestjs/common';
+import { BadRequestException, Logger, ValidationError, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,11 +14,27 @@ async function bootstrap() {
     new ValidationPipe({
       stopAtFirstError: true,
       exceptionFactory: (errors) => {
-        const firstError = Object.values(errors[0].constraints)[0]; // Pega apenas a primeira mensagem de erro
+        const firstError = findFirstConstraint(errors);
         return new BadRequestException(firstError);
       },
     }));
 
   await app.listen(process.env.PORT ?? 3000);
 }
+
+function findFirstConstraint(errors: ValidationError[]): string {
+  for (const error of errors) {
+    if (error.constraints && Object.values(error.constraints).length > 0) {
+      return Object.values(error.constraints)[0];
+    }
+
+    if (error.children && error.children.length > 0) {
+      const msg = findFirstConstraint(error.children);
+      if (msg) {
+        return msg;
+      }
+    }
+  }
+}
+
 bootstrap();
