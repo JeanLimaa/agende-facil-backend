785744bd49fd92c1eb256941bb31a54e2c9e7a8b
Jean Lima <jeansantoslima17@gmail.com>
2025-07-09 11:33:13 -0300

    feat(modules): refactor appointment and employee-services modules to import EmployeeModule and remove direct EmployeeService dependency

--- src/modules/appointments/appointment.module.ts
+++ src/modules/appointments/appointment.module.ts
@@ -4,10 +4,14 @@ import { AppointmentController } from './appointment.controller';
 import { AppointmentScheduler } from './appointment-scheduler.service';
 import { DatabaseService } from 'src/services/Database.service';
 import { EmployeeService } from '../employee/employee.service';
+import { EmployeeModule } from '../employee/employee.module';
 
 @Module({
+  imports: [
+    EmployeeModule
+  ],
   controllers: [AppointmentController],
-  providers: [AppointmentService, AppointmentScheduler, DatabaseService, EmployeeService],
+  providers: [AppointmentService, AppointmentScheduler, DatabaseService],
   exports: [AppointmentService],
 })
 export class AppointmentModule {}
--- src/modules/employee-services/employee-services.module.ts
+++ src/modules/employee-services/employee-services.module.ts
@@ -3,9 +3,13 @@ import { EmployeeService } from '../employee/employee.service';
 import { EmployeeServicesService } from './employee-services.service';
 import { DatabaseService } from 'src/services/Database.service';
 import { EmployeeServicesController } from './employee-services.controller';
+import { EmployeeModule } from '../employee/employee.module';
 
 @Module({
-    providers: [EmployeeServicesService, DatabaseService, EmployeeService],
+    imports: [
+        EmployeeModule
+    ],
+    providers: [EmployeeServicesService, DatabaseService],
     controllers: [EmployeeServicesController],
     exports: [EmployeeServicesService],
 })
