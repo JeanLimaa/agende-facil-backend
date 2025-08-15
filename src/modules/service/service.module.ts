220080017df3d4921d9d71cb9b99077563ec3e4b
Jean Lima <jeansantoslima17@gmail.com>
2025-08-15 20:41:33 -0300

    feat(service): enhance service creation with validation and error handling

--- src/modules/service/service.module.ts
+++ src/modules/service/service.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { ServiceController } from './service.controller';
 import { ServiceService } from './service.service';
 import { DatabaseService } from 'src/services/Database.service';
+import { CategoryModule } from '../category/category.module';
 
 @Module({
+  imports: [CategoryModule],
   controllers: [ServiceController],
   providers: [ServiceService, DatabaseService],
   exports: [ServiceService],
--- src/modules/service/service.service.ts
+++ src/modules/service/service.service.ts
@@ -1,14 +1,33 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { Prisma, Service } from '@prisma/client';
 import { DatabaseService } from 'src/services/Database.service';
+import { CategoryService } from '../category/category.service';
 
 @Injectable()
 export class ServiceService {
     constructor(
-        private readonly prisma: DatabaseService
+        private readonly prisma: DatabaseService,
+        private readonly categoriesService: CategoryService
     ) {}
 
     public async create(data: Prisma.ServiceCreateManyInput): Promise<Prisma.ServiceCreateManyInput> {
+        const existingService = await this.prisma.service.findFirst({
+            where: {
+                name: data.name,
+                companyId: data.companyId
+            }
+        });
+
+        if (existingService) {
+            throw new BadRequestException('Serviço com esse nome já existe');
+        }
+
+        const existCategory = await this.categoriesService.getCategoryById(data.categoryId);
+
+        if (!existCategory) {
+            throw new BadRequestException('Categoria não encontrada');
+        }
+
         return await this.prisma.service.create({
             data
         });
